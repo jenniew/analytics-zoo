b90bec7874893b5776c760b5f288fc07d300b949
jenniew <jenniewang123@gmail.com>
2020-10-29 14:16:27 -0700

    update size

--- zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/models/Topology.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/pipeline/api/keras/models/Topology.scala
@@ -1815,7 +1815,7 @@ object InternalDistriOptimizer {
 //        (0 until extraParamLength).foreach(i =>
 //          extraState(i) = models.map(_.localModels.head.getExtraParameter()(i)).first()
 //        )
-        val extraState = collectExtraParameters[T](models, trainingModel, maxSize = Integer.MAX_VALUE - 8)
+        val extraState = collectExtraParameters[T](models, trainingModel, maxSize = Integer.MAX_VALUE / 2)
         trainingModel.setExtraParameter(extraState)
 //        val extraState = models.map(_.localModels.head.getExtraParameter()).first()
         trainingModel.setExtraParameter(extraState)
